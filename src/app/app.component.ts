c91260a4ff6a7bb1b78aec6c97e323210c7bf384
Perry Brandiezs <pbrandiezs@gmail.com>
2021-09-29 09:06:16 -0700

    first commit - observables example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { filter, debounceTime, distinctUntilChanged } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <input class="form-control" type="search" [formControl]="searchControl" />
+  `,
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'observables-example';
+  searchControl = new FormControl();
+
+  constructor() {
+    this.searchControl.valueChanges
+      .pipe(
+        filter((text) => text.length >= 3),
+        debounceTime(400),
+        distinctUntilChanged()
+      )
+      .subscribe((value) => {
+        console.log(value);
+      });
+  }
 }
